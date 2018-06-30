634e2e5ffe976ff8a5f4aaf6c253dee1354cfb01
Mikhail <myakushev2@gmail.com>
2018-07-01 02:26:01 +0300

    3rd branch

--- src/main/java/com/yak/test/TestMain.java
+++ src/main/java/com/yak/test/TestMain.java
@@ -5,5 +5,6 @@ public class TestMain {
         System.out.println("Hellow World!");
         System.out.println("1st concurrent");
         System.out.println("2nd concurent");
+        System.out.println("Begin working in 3rd branch");
     }
 }
