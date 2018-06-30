92793670fc63f4355a1a0920a38021dd90e80c26
Mikhail <myakushev2@gmail.com>
2018-07-01 01:59:20 +0300

    1st concurrent

--- src/main/java/com/yak/test/TestMain.java
+++ src/main/java/com/yak/test/TestMain.java
@@ -3,5 +3,6 @@ package com.yak.test;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hellow World!");
+        System.out.println("1st concurrent");
     }
 }
