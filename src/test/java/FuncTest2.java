cfb44ae293110eda60ee08666655068e29014254
Mikhail <myakushev2@gmail.com>
2018-07-26 01:12:39 +0300

    5-1-First-branck-to-test-merge-conflicts

---
+++ src/test/java/FuncTest2.java
@@ -0,0 +1,42 @@
+import com.yak.test.TestsMethods;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FuncTest2 {
+    private TestsMethods tm;
+
+    @Before
+    public void init() {
+        tm = new TestsMethods();
+    }
+
+    @After
+    public void tearDown() {
+        tm = null;
+    }
+
+    @Test
+    public void checkSumm1() {
+        assertEquals(12, tm.summ(11,1));
+    }
+
+    @Test
+    public void checkSumm2() {
+        assertEquals(0, tm.summ(2,-2));
+    }
+
+    @Test
+    public void checkSumm3() {
+        assertEquals(10, tm.summ(-11,21));
+    }
+
+    @Test
+    public void checkSumm4() {
+        assertEquals(-9, tm.summ(11,-20));
+    }
+
+    //this is 1
+}
